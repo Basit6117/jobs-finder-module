d7199160b0c9943351268b1f30519288e9b3889b
Abdul Basit <abdul.b6117@gmail.com>
2025-09-12 11:56:48 +0500

    Added location and time filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,36 +1,53 @@
 import "../styles/Filters.css"
-const Filters = ({filter, setFilter,setLoading}) => {
-   function handleOptions(e){
-    setLoading(true)
-     setFilter(e.target.value)
-     setLoading(false)
-   }
+const Filters = (
+  { filter, 
+    setFilter, 
+    setSelectedLocation, 
+    selectedLocation, 
+    jobs ,
+    timeFilter,
+    setTimeFilter
+  }) => {
+  function handleOptions(e) {
+    setFilter(e.target.value)
+
+  }
+  function handleLocOptions(e) {
+    setSelectedLocation(e.target.value)
+  }
+  const uniqueLocations = [
+    ...new Set(
+      jobs.flatMap(job =>
+        job.candidate_required_location
+          .split(",")
+          .map(loc => loc.trim())
+      )
+    )
+  ];
   return (
     <>
-        <select className='select-section' name="" id="">
-          <option value="">All</option>
-          <option value="">Africa</option>
-          <option value="">Americas</option>
-          <option value="">Asia</option>
-          <option value="">Oceania</option>
-          <option value="">Europe</option>
-        </select>
-        <select className='select-section' name="" id="">
-          <option disabled selected value="all">Work Mode</option>
-          <option value="all">All</option>
-          <option value="Remote">Remote</option>
-          <option value="On-site">On-site</option>
-          <option value="Hybrid">Hybrid</option>
-        </select>
-        <select value={filter} onChange={handleOptions} className='select-section' name="" id="">
-          <option disabled selected value="all" >Job Type</option>
-          <option value="all">All</option>
-          <option value="internship">Internship</option>
-          <option value="contract">Contract</option>
-          <option value="full_time">Full Time</option>
-          <option value="part_time">Part Time</option>
-        </select>
-</>
+      <select value={selectedLocation} onChange={handleLocOptions} className='select-section' name="" id="">
+        <option value="all">All Locations</option>
+        {uniqueLocations.map((loc, index) => (
+          <option key={index} value={loc}>{loc}</option>
+        ))}
+      </select>
+      <select value={timeFilter} onChange={(e)=>setTimeFilter(e.target.value)} className='select-section' name="" id="">
+        <option disabled selected value="all">Any Date</option>
+        <option value="all">All</option>
+        <option value="24h">Last 24 hours</option>
+        <option value="7d">Last 7 days</option>
+        <option value="30d">Last 30 days</option>
+      </select>
+      <select value={filter} onChange={handleOptions} className='select-section' name="" id="">
+        <option disabled selected value="all" >Job Type</option>
+        <option value="all">All</option>
+        <option value="internship">Internship</option>
+        <option value="contract">Contract</option>
+        <option value="full_time">Full Time</option>
+        <option value="part_time">Part Time</option>
+      </select>
+    </>
   )
 }
 
--- src/pages/JobsPage.jsx
+++ src/pages/JobsPage.jsx
@@ -12,10 +12,13 @@ const JobsPage = () => {
   const [loading, setLoading] = useState(false)
   const [search, setSearch] = useState("")
   const [filter, setFilter] = useState("all")
+  const [selectedLocation, setSelectedLocation] = useState("all")
+  const [timeFilter, setTimeFilter] = useState("all");
+
 
   useEffect(() => {
     setLoading(true)
-    fetch("https://remotive.com/api/remote-jobs?limit=50")
+    fetch("https://remotive.com/api/remote-jobs?limit=500")
       .then(res => res.json())
       .then(data => {
         setJobs(data.jobs)
@@ -42,16 +45,57 @@ if(filter === 'all'){
 }
 }
 
+//location search
+const filterByLocation = (job) => {
+  if (selectedLocation === "all") return true;
+  return job.candidate_required_location
+           .toLowerCase()
+           .includes(selectedLocation.toLowerCase());
+};
+ //time filter
+const filterByTime = (job) => {
+  if (timeFilter === "all") return true;
+
+  const jobDate = new Date(job.publication_date);
+  const today = new Date();
+
+  if (timeFilter === "24h") {
+    return (today - jobDate) / (1000 * 60 * 60) <= 24;
+  } 
+  if (timeFilter === "7d") {
+    return (today - jobDate) / (1000 * 60 * 60 * 24) <= 7;
+  }
+  if (timeFilter === "30d") {
+    return (today - jobDate) / (1000 * 60 * 60 * 24) <= 30;
+  }
+
+  return true;
+};
+
+
 
   //search and filter logic
-  const filterJobs = jobs.filter((indJob) => searchJob(indJob) && filterJobType(indJob)
+  const filterJobs = jobs.filter((indJob) => 
+    searchJob(indJob)
+  && filterJobType(indJob)
+  && filterByLocation(indJob)
+  && filterByTime(indJob)
    )
   console.log(jobs)
   return (
     <>
       <header className='header-container'>
         <Searchbar search={search} setSearch={setSearch} setLoading={setLoading} />
-        <Filters filter={filter} setFilter={setFilter} setLoading={setLoading}/>
+        <Filters 
+        filter={filter} 
+        setFilter={setFilter} 
+        setLoading={setLoading} 
+        jobs={jobs}
+        selectedLocation={selectedLocation}
+        setSelectedLocation={setSelectedLocation}
+        timeFilter={timeFilter}
+        setTimeFilter={setTimeFilter}
+        />
       </header>
       <main>
        <h1>Explore Opportunities {filterJobs.length}</h1>
